78993fa0f9b54c031f89829ca4bbee66ce02b05f
yuriy-aza <yuriy.aza@gmail.com>
2023-03-15 22:18:28 +0200

    Task-02

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,10 @@
 const categoriesListRef = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesListRef.length} \n\n`);
 
-categoriesListRef.forEach((item) => {
-  const categoryHeaderRef = item.querySelector('h2');
+categoriesListRef.forEach((element) => {
+  const categoryHeaderRef = element.querySelector('h2');
   console.log(`Category: ${categoryHeaderRef.textContent}`);
 
-  const categoryElementsRef = item.querySelector('ul');
+  const categoryElementsRef = element.querySelector('ul');
   console.log(`Elements: ${categoryElementsRef.children.length} \n\n`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elementList = ingredients.map((element) => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = element;
+  itemRef.classList.add('item');
+  return itemRef;
+});
+
+const ingredientsListRef = document.querySelector('#ingredients');
+ingredientsListRef.append(...elementList);
+
+console.log(ingredientsListRef);
+
+// Альтернативний спосіб
+
+const elementListAlternate = ingredients
+  .map((element) => `<li class="item">${element}</li>`)
+  .join(' ');
+  
+const markupAlternate = `
+  <ul id="ingredients-alternative" style="color:blue">
+    ${elementListAlternate}
+  </ul>`;
+
+ingredientsListRef.insertAdjacentHTML('afterEnd', markupAlternate);
